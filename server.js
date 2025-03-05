09b8df33860d40349eb544253e635ee79fc65e32
domingos117 <suyannegomes01@gmail.com>
2025-03-05 09:06:38 -0300

    ~~

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express()
+const bcrypt = require('bcrypt');
+
+app.use(express.json())
+
+const users = [];
+app.get('/users', (req, res) => {
+    res.json(users)
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = { name: req.body.name, password: hashedPassword };
+        users.push(user);
+        res.status(200).send();
+
+    }
+    catch {
+        res.status(500).send();
+    }
+});
+
+app.listen(3000)
